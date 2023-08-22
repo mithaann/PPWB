28c2bb7ed0ae2b052c77998d722133cfd8870def
karma <89170731+coolguy916@users.noreply.github.com>
2023-08-22 11:10:57 +0700

    first commit

---
+++ Package/CRUD Kategori/view/kategori/index.blade.php
@@ -0,0 +1,110 @@
+@extends('layouts.dashboard')
+@section('content')
+<div class="container-fluid">
+  <!-- table kategori -->
+  <div class="row">
+    <div class="col">
+      <div class="card">
+        <div class="card-header">
+          <h4 class="card-title">Kategori Produk</h4>
+          <div class="card-tools">
+            <a href="{{ route('kategori.create') }}" class="btn btn-sm btn-primary">
+              Baru
+            </a>
+          </div>
+        </div>
+        <div class="card-body">
+          <form action="#">
+            <div class="row">
+              <div class="col">
+                <input type="text" name="keyword" id="keyword" class="form-control" placeholder="ketik keyword disini">
+              </div>
+              <div class="col-auto">
+                <button class="btn btn-primary">
+                  Cari
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="card-body">
+          @if ($message = Session::get('error'))
+              <div class="alert alert-warning">
+                  <p>{{ $message }}</p>
+              </div>
+          @endif
+          @if ($message = Session::get('success'))
+              <div class="alert alert-success">
+                  <p>{{ $message }}</p>
+              </div>
+          @endif
+          <div class="table-responsive">
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th width="50px">No</th>
+                  <th>Gambar</th>
+                  <th>Kode</th>
+                  <th>Nama</th>
+                  <th>Jumlah Produk</th>
+                  <th>Status</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+              @foreach($itemkategori as $kategori)
+                <tr>
+                  <td>
+                  {{ ++$no }}
+                  </td>
+                  <td>
+                    <img src="{{ asset('images/slide1.jpg') }}" alt="kategori 1" width='150px'>
+                    <div class="row mt-2">
+                      <div class="col">
+                        <input type="file" name="gambar" id="gambar">
+                      </div>
+                      <form action="{{ url('/admin/kategorimage') }}" method="post" style="display:inline;">
+                      @csrf
+                      {{ method_field('uploadimage') }}
+                      <button type="submit" class="btn btn-sm btn-danger mb-2">
+                        upload
+                      </button>                    
+                    </form>
+                  </td>
+                  <td>
+                  {{ $kategori->kode_kategori }}
+                  </td>
+                  <td>
+                  {{ $kategori->nama_kategori }}
+                  </td>
+                  <td>
+                  {{ is_countable($kategori->produk) }}
+                  </td>
+                  <td>
+                  {{ $kategori->status }}
+                  </td>
+                  <td>
+                    <a href="{{ route('kategori.edit', $kategori->id) }}" class="btn btn-sm btn-primary mr-2 mb-2">
+                      Edit
+                    </a>
+                    <form action="{{ route('kategori.destroy', $kategori->id) }}" method="post" style="display:inline;">
+                      @csrf
+                      {{ method_field('delete') }}
+                      <button type="submit" class="btn btn-sm btn-danger mb-2">
+                        Hapus
+                      </button>                    
+                    </form>
+                  </td>
+                </tr>
+              @endforeach
+              </tbody>
+            </table>
+            <!-- untuk menampilkan link page, tambahkan skrip di bawah ini -->
+            {{ $itemkategori->links() }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
