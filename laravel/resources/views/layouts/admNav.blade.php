d8bb774bfb5c67b66d09b3fdde961d97377c9f69
KrissYus <kristinayusendra@gmail.com>
2023-10-09 12:10:10 +0700

    admin

--- laravel/resources/views/layouts/admNav.blade.php
+++ laravel/resources/views/layouts/admNav.blade.php
@@ -9,14 +9,11 @@
                 <b class="logo-icon ps-2">
                     <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
                     <!-- Dark Logo icon -->
-                    <img src="{{ asset('assets/images/logo-icon.png') }}" alt="homepage" class="light-logo" width="25" />
+                    <img src="{{ asset('images/logom.png') }}" alt="homepage" class="light-logo" width="150" />
                 </b>
                 <!--End Logo icon -->
                 <!-- Logo text -->
-                <span class="logo-text ms-2">
                     <!-- dark Logo text -->
-                    <img src="{{ asset('assets/images/logo-text.png') }}" alt="homepage" class="light-logo" />
-                </span>
                 <!-- Logo icon -->
                 <!-- <b class="logo-icon"> -->
                 <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
@@ -46,26 +43,6 @@
                 <li class="nav-item d-none d-lg-block">
                     <a class="nav-link sidebartoggler waves-effect waves-light" href="javascript:void(0)"
                         data-sidebartype="mini-sidebar"><i class="mdi mdi-menu font-24"></i></a>
-                </li>
-                <!-- ============================================================== -->
-                <!-- create new -->
-                <!-- ============================================================== -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        <span class="d-none d-md-block">Create New <i class="fa fa-angle-down"></i></span>
-                        <span class="d-block d-md-none"><i class="fa fa-plus"></i></span>
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </li>
-                    </ul>
                 </li>
                 <!-- ============================================================== -->
                 <!-- Search -->
