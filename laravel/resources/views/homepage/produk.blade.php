6709d8958ee93d70e514b700c6e85df6d17ee665
yonathanash <orimuraichika991@gmail.com>
2023-10-12 10:28:59 +0700

    fixed

--- laravel/resources/views/homepage/produk.blade.php
+++ laravel/resources/views/homepage/produk.blade.php
@@ -9,27 +9,22 @@
       </div>
       <div class="row">
         @foreach( $produk as $p)
+        @if($loop->iteration <= 4)
         <div class="col-sm-6 col-md-4 col-lg-3">
-          <div class="box">
-            <a href="{{ route('item_content')}}">
-              <div class="img-box">
-                <img src="{{asset('storage/fruit/'. $p->buah_gambar)}}" alt="">
-              </div>
-              <div class="detail-box">
-                <h6>
-                  {{ $p->buah_nama}}
-                </h6>
-                <h6>
-                  Rp.
-                  <span>
-                    {{ $p->harga}}
-                  </span>
-                </h6>
-              </div>
-              <img src="{{asset('storage/country/'. $p->negara->negara_image)}}" alt="" class="new">
-            </a>
-          </div>
+            <div class="box">
+                <a href="{{ route('item_content') }}">
+                    <div class="img-box">
+                        <img src="{{ asset('storage/fruit/' . $p->buah_gambar) }}" alt="">
+                    </div>
+                    <div class="detail-box">
+                        <h6>{{ $p->buah_nama }}</h6>
+                        <h6>Rp. <span>{{ $p->harga }}</span></h6>
+                    </div>
+                    <img src="{{ asset('storage/country/' . $p->negara->negara_image) }}" alt="" class="new">
+                </a>
+            </div>
         </div>
+    @endif
         @endforeach
 
         
