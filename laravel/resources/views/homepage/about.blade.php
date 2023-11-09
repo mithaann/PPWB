605a31c009b1cae865df16e0aa8340a6f1b80450
mithaann <143058510+mithaann@users.noreply.github.com>
2023-11-09 10:40:28 +0700

    update about

--- laravel/resources/views/homepage/about.blade.php
+++ laravel/resources/views/homepage/about.blade.php
@@ -9,14 +9,16 @@
         </h2>
       </div>
       <div class="img-box">
-        <img src="images/fruit image.png" alt="">
+        <img src="images/aboutbuah.png" alt="">
       </div>
       <div class="detail-box">
         <p>
-          It is a long established fact that a reader will be distracted by the readable content of a page when looking
-          at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as
-          opposed to using 'Content here, content here', making it
-        </p>
+        Feefruit adalah sebuah platform e-commerce yang khusus menghadirkan transaksi jual beli buah impor dan lokal. Situs ini dirancang dengan struktur yang efisien untuk memudahkan navigasi pengguna. Halaman utama menampilkan penawaran terbaru dan rekomendasi buah sesuai musim. Kategori buah tersusun rapi, meliputi buah eksotis, 
+        tropis, dan musiman, dengan informasi detail, gambar, asal, dan manfaat kesehatan yang disertakan.</p>
+        <p> Pencarian dan filter membantu pengguna menemukan buah berdasarkan kriteria tertentu, seperti jenis, harga, atau asal. Fitur keranjang belanja memungkinkan penambahan produk secara langsung. Proses pembelian dilakukan secara intuitif dan efisien. Sistem akun memberikan pengalaman personalisasi dengan pelacakan pesanan dan riwayat pembelian. 
+          Metode pembayaran meliputi kartu kredit, transfer bank, dan dompet digital, dengan keamanan terjamin.</p>
+          <p>Layanan pelanggan terintegrasi melalui nomor telepon, email, dan live chat untuk respons cepat terhadap pertanyaan atau masalah. Halaman blog memberikan informasi tambahan mengenai jenis buah, tips memilih, dan resep berbasis buah. 
+            Feefruit memastikan edukasi pelanggan dan interaksi lebih dalam.</p>
         <div class="d-flex justify-content-center">
           <a href="/">
             back
